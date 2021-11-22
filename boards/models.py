9dc1505574eed361f89dd80caae1dc2975f12712
juanolaechea <42986554+juanolaechea@users.noreply.github.com>
2021-11-22 20:17:24 -0300

    signal

--- boards/models.py
+++ boards/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.timezone import now
+from django.db.models.signals import pre_save,pre_delete
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -23,3 +25,15 @@ class Task(models.Model):
     state = models.CharField(max_length=15, choices=states, default='To-Do')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+@receiver(pre_save, sender=Task)
+def pre_save_task(sender, instance, **kwargs):
+    if not instance._state.adding:
+        print('Se actualizo una Tarea')
+    else:
+        print('Se agrego una nueva Tarea')
+
+
+@receiver(pre_delete, sender=Task)
+def pre_delete_task(sender, instance, **kwargs):
+   print('Tarea eliminada')
